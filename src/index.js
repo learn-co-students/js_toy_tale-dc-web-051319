b6acb37dbcda506c3778773505dfb59290aef17e
Matt Williams <matt.seongjun@gmail.com>
2019-06-27 16:08:57 -0400

    add render toys fn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,39 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
+function renderToys(toys) {
+    let toyCollection = document.querySelector("#toy-collection")
+
+    toys.forEach(toy => {
+        //make a card
+        let toyCard = document.createElement("div");
+        toyCard.classList.add("card");
+        toyCard.id = `toy-${toy.id}`
+
+        //make and append name
+        let toyName = document.createElement("h2");
+        toyName.innerText = toy.name;
+        toyCard.appendChild(toyName);
+
+        //make and append image
+        let toyImage = document.createElement("ing");
+        toyImage.src = toy.image;
+        toyCard.appendChild(toyImage);
+
+        //make and append likes
+        let toyLikes = document.createElement("p");
+        toyLikes.innerText = toy.likes;
+        toyCard.appendChild(toyLikes);
+
+        //append card
+        toyCollection.appendChild(toyCard);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    let toys = fetchToys();
+    renderToys(toys);
+});
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -14,6 +46,3 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
-
-
-// OR HERE!
