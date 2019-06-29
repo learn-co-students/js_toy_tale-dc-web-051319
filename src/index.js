cb54c2392e82a49deaf26f208f1d8bd3a66f0023
github username <github email address>
2019-06-29 18:25:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,79 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+document.addEventListener('DOMContentLoaded', () => {
+  // Event Listeners go here
+  // Starts the fetch
+  const addBtn = document.querySelector('#new-toy-btn')
+  // cd 
 
-// YOUR CODE HERE
+  const toyForm = document.querySelector('.container')
+  let addToy = false
+  fetchToys()
+  
 
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-    // submit listener here
-  } else {
-    toyForm.style.display = 'none'
+});
+
+
+
+
+
+  
+
+
+function toyCollect(){
+  let toyCollection = document.querySelector('#toy-collection')
+  return toyCollection
+}
+
+
+function fetchToys(){
+  fetch('http://localhost:3000/toys')         //Fetches URL
+  .then(response => response.json())          //Gets Response in 
+  .then(toyData => {
+      toyData.forEach(toy => {createToys(toy)})
+     
   }
-})
+  )};
+
+function createToys(toy){
+  
+  let card = document.createElement('div')
+  let h2 = document.createElement('h2')
+  let img = document.createElement('img')
+  let p = document.createElement('p')
+  p.innerText = ""+toy.likes+" Likes"
+  let button = document.createElement('button')
+  button.setAttribute('class', 'like-btn')
+  button.innerText = 'Like <3'
+  h2.innerText = ""+toy.name+""
+  img.setAttribute('src', ""+toy.image+"")
+  img.setAttribute('class', 'toy-avatar')
+  card.setAttribute('class', 'card')
+  card.append(h2) 
+  card.append(img)
+  card.append(p)
+  card.append(button)
+  renderToys(card)
+  
+}
+
+function renderToys(card){
+  toyCollect().append(card)
+}
 
 
 // OR HERE!
+
+
+// addBtn.addEventListener('click', () => {
+ 
+
+
+
+//   // hide & seek with the form
+//   addToy = !addToy
+//   if (addToy) {
+//     toyForm.style.display = 'block'
+//     // submit listener here
+//   } else {
+//     toyForm.style.display = 'none'
+//   }
+// })
